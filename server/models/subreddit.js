c9994b44a61650e5860062ff087e0d72c39a9ac5
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-02-13 15:19:26 +0530

    Updated schemas

--- server/models/subreddit.js
+++ server/models/subreddit.js
@@ -1,38 +1,43 @@
 const mongoose = require('mongoose')
 const schemaCleaner = require('../utils/schemaCleaner')
 
-const subredditSchema = new mongoose.Schema({
-    subredditName : {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description : {
-        type: String,
-        required: true,
-        trim: true
-    },
-    posts : [
-        {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : 'Post'
-        }
-    ],
-    admin : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    subscribedBy : [
-        {
+const subredditSchema = new mongoose.Schema(
+    {
+        subredditName : {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description : {
+            type: String,
+            required: true,
+            trim: true
+        },
+        posts : [
+            {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : 'Post'
+            }
+        ],
+        admin : {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
+        },
+        subscribedBy : [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        subscribersCount : {
+            type: Number,
+            required: true
         }
-    ],
-    subscribersCount : {
-        type: Number,
-        required: true
+    },
+    {
+        timestamps : true
     }
-})
+)
 
 schemaCleaner(subredditSchema)
 module.exports = mongoose.model('Subreddit', subredditSchema)
