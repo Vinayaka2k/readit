f4016242505a08d030d7f13526d68efcf2e79335
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-02-14 14:50:59 +0530

    Added API for deleting a reply to comment from posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -193,8 +193,72 @@ router.patch('/:id/comment/:commentId', auth, async (req, res) => {
     
     targetComment.commentBody = req.body.comment
     post.comments = post.comments.map( c => c._id.toString() !== req.params.commentId ? c : targetComment)
-    await post.save()
-    res.status(202).json(post)
+    const savedPost = await post.save()
+    const populatedPost = savedPost.populate([
+        {path: 'author', select: 'username'},
+        {path: 'subreddit', select: 'subredditName'},
+        {path: 'comments.commentedBy', select: 'username'},
+        {path: 'comments.replies.repliedBy', select: 'username'}
+    ])
+    res.status(202).json(populatedPost)
 })
+
+router.post('/:id/comment/:commentId/reply', auth, async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    const user = await User.findById(req.user)
+    if(!req.body.reply)
+        return res.status(400).send({message: 'Reply body cant be empty'})
+    if(!post)
+        return res.status(404).send({message: `Post with ${req.params.id} doesnt exist in DB`})
+    if(!user)
+        return res.status(404).send({message: `User doesnot exits in database`})
+    
+    const targetComment = post.comments.find( c => c._id.toString() === req.params.commentId)
+    if(!targetComment)
+        return res.status(404).send({message: `Comment with id ${req.params.commentId} doesnt exist in database`})
+    
+    targetComment.replies = targetComment.replies.concat({
+        replyBody : req.body.reply,
+        repliedBy: user._id
+    })
+
+    post.comments = post.comments.map( c => c._id.toString() !== req.params.commentId ? c : targetComment)
+    const savedPost = await post.save()
+    const populatedPost = savedPost.populate([
+        {path: 'author', select: 'username'},
+        {path: 'subreddit', select: 'subredditName'},
+        {path: 'comments.commentedBy', select: 'username'},
+        {path: 'comments.replies.repliedBy', select: 'username'}
+    ])
+    res.status(201).json(populatedPost)
+})
+
+router.delete('/:id/comment/:commentId/reply/:replyId', auth, async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    const user = await User.findById(req.user)
+    
+    if(!post)
+        return res.status(404).send({message: `Post with ${req.params.id} doesnt exist in DB`})
+    if(!user)
+        return res.status(404).send({message: `User doesnot exits in database`})
+    
+    const targetComment = post.comments.find( c => c._id.toString() === req.params.commentId)
+    if(!targetComment)
+        return res.status(404).send({message: `Comment with id ${req.params.commentId} doesnt exist in database`})
+    
+    const targetReply = targetComment.replies.find( r => r._id.toString() === req.params.replyId)
+    if(!targetReply)    
+        return res.status(404).send({message: `Reply to Comment with id ${req.params.replyId} doesnt exist in database`})
+    
+    if(targetReply.repliedBy.toString() != user._id.toString())
+        return res.status(401).send({message: 'Access is denied'})
+    
+    targetComment.replies = targetComment.replies.filter(r => r._id.toString() !== req.params.replyId)
+    post.comments = post.comments.map( c => c._id.toString() !== req.params.commentId ? c : targetComment)
+    await post.save()
+    return res.status(204).end()
+})
+
+
 
 module.exports = router
